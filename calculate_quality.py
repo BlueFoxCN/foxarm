913b3e22f3f1a9bd9218af6f1d7688dce2285ae0
JesseYang <jesse.yang@1985@gmail.com>
2018-08-10 09:25:00 +0800

    calculate quality

---
+++ calculate_quality.py
@@ -0,0 +1,34 @@
+import copy
+import os
+import trimesh
+from autolab_core import RigidTransform, YamlConfig
+
+from foxarm.grasping.grasp import ParallelJawPtGrasp3D
+from foxarm.grasping.gripper import RobotGripper
+from foxarm.grasping.grasp_sampler import GraspSampler, AntipodalGraspSampler
+from foxarm.grasping.graspable_object import GraspableObject, GraspableObject3D
+from foxarm.constants import *
+from foxarm.grasping.contacts import Contact, Contact3D
+from foxarm.grasping.quality import PointGraspMetrics3D
+from foxarm.common import constants
+from foxarm.common.keys import *
+from foxarm.common.sdf_file import SdfFile
+
+obj_path = "mini_dexnet/bar_clamp.obj"
+sdf_path = "mini_dexnet/bar_clamp.sdf"
+mesh = trimesh.load_mesh(obj_path)
+sdf = SdfFile(sdf_path).read()
+
+CONFIG = YamlConfig(TEST_CONFIG_NAME)
+
+obj = GraspableObject3D(sdf, mesh)
+gripper = RobotGripper.load(GRIPPER_NAME, os.path.join(WORK_DIR, "foxarm/common"))
+ags = AntipodalGraspSampler(gripper, CONFIG)
+
+##### Generate Grasps #####
+unaligned_grasps = ags.generate_grasps(obj, target_num_grasps=100)
+print('### Generated %d unaligned grasps! ###' % len(unaligned_grasps))
+grasps = {}
+
+# calculate quality
+# PointGraspMetrics3D.grasp_quality(unaligned_grasps[0], obj)
