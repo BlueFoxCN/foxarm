3a2e333453437b0fa455bf592c267e1c6ab29a0d
JesseYang <jesse.yang1985@gmail.com>
2018-08-29 06:33:42 +0800

    generate sdf

--- sdf_gen.py
+++ sdf_gen.py
@@ -7,6 +7,9 @@ obj_path_list_2 = [os.path.join('objs/kit', e) for e in os.listdir('objs/kit')]
 obj_path_list = obj_path_list_1 + obj_path_list_2
 
 for idx, obj_path in enumerate(obj_path_list):
+    sdf_path = obj_path[:-4] + ".sdf"
+    if os.path.isfile(sdf_path):
+        continue
     cmd = 'SDFGen %s 100 5' % obj_path
     if idx % 10 == 0:
         print(idx)
